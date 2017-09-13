a606a90f304735f3ea3644317ce76af37264a462
Brown, Chris <chbrown@nthrive.com>
2017-09-13 12:07:48 -0500

    Adding Formatters

---
+++ Testing/Common.Formaters/Convertors/ConcreteTypeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Common.Formaters.Convertors
+{
+    public class ConcreteTypeConverter<TInterface, TConcrete> : JsonConverter
+    {
+        /// <summary>
+        ///     Can Convert
+        /// </summary>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(TInterface);
+        }
+
+        /// <summary>
+        ///     Read Json
+        /// </summary>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            return serializer.Deserialize<TConcrete>(reader);
+        }
+
+        /// <summary>
+        ///     Write Json
+        /// </summary>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+    }
+}
