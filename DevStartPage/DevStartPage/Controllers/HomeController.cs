d61c96fbebc502aabcd1cec978422633f867857f
Chris Brown <chbrown@nthrive.com>
2018-01-05 15:38:41 -0600

    Adding Start Page Base

---
+++ DevStartPage/DevStartPage/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+
+namespace TestingSite.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var userIpAddress = GetIPAddress();
+			ViewBag.Message = userIpAddress;
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		public ActionResult Downloads()
+		{
+			ViewBag.Message = "Downloads";
+
+			return View();
+		}
+
+		protected string GetIPAddress()
+		{
+			System.Web.HttpContext context = System.Web.HttpContext.Current;
+			string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+			if (!string.IsNullOrEmpty(ipAddress))
+			{
+				string[] addresses = ipAddress.Split(',');
+				if (addresses.Length != 0)
+				{
+					return addresses[0];
+				}
+			}
+
+			return context.Request.ServerVariables["REMOTE_ADDR"];
+		}
+	}
+}
