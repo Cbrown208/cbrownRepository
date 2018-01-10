00cbef652901f9c3632e8210ef5a8c2c05fc6f62
Chris Brown <chbrown@nthrive.com>
2018-01-10 08:33:04 -0600

    updating Deployment for IIS

--- DevStartPage/DevStartPage.Web/App_Start/RouteConfig.cs
+++ DevStartPage/DevStartPage.Web/App_Start/RouteConfig.cs
@@ -12,8 +12,9 @@ namespace DevStartPage.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+	        namespaces: new[] { "DevStartPage.Web" }
+			);
         }
     }
 }
---
+++ DevStartPage/DevStartPage.Web/NpmInstall-DevStartPage.bat
@@ -0,0 +1,3 @@
+::Deploy Website
+
+gulp build:prod
