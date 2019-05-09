6f122ebb8f4d20a0f84b67813b2e9cb0b213b4e1
Brown, Chris <chbrown@nthrive.com>
2019-05-09 14:57:13 -0500

    Refactoring Send Email Methods

---
+++ Tools/Tools.SendingEmail/Models/CopyDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace Tools.SendingEmail.Models
+{
+	public class CopyDetails
+	{
+		public string Total { get; set; }
+		public string Copied { get; set; }
+		public string Skipped { get; set; }
+		public string Mismatched { get; set; }
+		public string Failed { get; set; }
+		public string Extras { get; set; }
+	}
+}
---
+++ Tools/Tools.SendingEmail/Models/LogStats.cs
@@ -0,0 +1,10 @@
+﻿namespace Tools.SendingEmail.Models
+{
+	public class LogStats
+	{
+		public LogStatsDetails Documents { get; set; }
+		public LogStatsDetails MyScripts { get; set; }
+		public LogStatsDetails Pictures { get; set; }
+		public LogStatsDetails Music { get; set; }
+	}
+}
